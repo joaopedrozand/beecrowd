66818463d5cdbce149909feeb936aefaf90af691
João Pedro Zandoná <33556037+joaopedrozand@users.noreply.github.com>
2020-12-25 16:44:33 -0300

    Create 1002.c

---
+++ 1002.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+    int main(){           
+		double raio,area;     
+	 	double pi = 3.14159;       
+	    printf("");   
+		scanf("%lf",&raio);   
+		area = pi * (raio * raio);  
+        printf("A=%.4lf\n",area);             
+		return 0;       
+	}
