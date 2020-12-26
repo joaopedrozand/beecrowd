74ca7271e7c981b5ee335244a88222b9c684aea2
João Pedro Zandoná <33556037+joaopedrozand@users.noreply.github.com>
2020-12-25 21:30:46 -0300

    Create 1013_alt.c

---
+++ 1013_alt.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int maior(int a,int b){
+    return (a+b+abs(a-b))/2;
+}
+int main(){
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    printf("%d eh o maior\n",maior(maior(a,b),c));
+    return 0;
+}
