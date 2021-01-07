dd58959b52e9070e8a31a7c3c3352bdc1063a35a
João Pedro Zandoná <33556037+joaopedrozand@users.noreply.github.com>
2021-01-07 13:00:57 -0300

    Create 1072.c

---
+++ c/1072.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n,in=0,out=0,x,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&x);
+        if(x>=10&&x<=20)in++;
+        else out++;
+    }
+    printf("%d in\n%d out\n",in,out);
+    return 0;
+}
