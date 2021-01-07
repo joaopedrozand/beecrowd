8679a97c1cd7e24c7b56d536eae201f0d2794c5a
João Pedro Zandoná <33556037+joaopedrozand@users.noreply.github.com>
2021-01-07 12:51:37 -0300

    Create 1071.c

---
+++ c/1071.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int x,y,i,aux,soma=0;
+    scanf("%d %d",&x,&y);
+    if(x>y){
+        aux = x;
+        x = y;
+        y = aux;
+    }
+    for(i=x+1;i<y;i++){
+        if(i%2!=0){
+            soma+=i;
+        }
+    }
+    printf("%d\n",soma);
+    return 0;
+}
