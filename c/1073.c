a34d133c0741da6db5d33a059d183e618ed97a42
João Pedro Zandoná <33556037+joaopedrozand@users.noreply.github.com>
2021-01-07 13:22:43 -0300

    Create 1073.c

---
+++ c/1073.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int i,n;
+    scanf("%d",&n);
+    if(n>5&&n<2000)
+        for(i=1;i<=n;i++){
+            if(i%2==0) {
+                printf("%d^2 = %lu\n", i, i*i);
+            }
+        }
+    return 0;
+}
