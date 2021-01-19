c577ade7df69ab721300586b29161bf5523ca50a
João Pedro Zandoná <33556037+joaopedrozand@users.noreply.github.com>
2021-01-19 15:03:55 -0300

    Create 1173.c

---
+++ c/1173.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+    int x[10],i;
+    scanf("%d",&x[0]);
+    for(i=1;i<10;i++) {
+        x[i] = x[i-1]*2;
+    }
+    for(i=0;i<10;i++)
+        printf("N[%d] = %d\n",i,x[i]);
+    return 0;
+}
