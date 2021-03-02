8896169e706f48e0aca91337318adc2fcc5593bd
João Pedro Zandoná <33556037+joaopedrozand@users.noreply.github.com>
2021-03-02 20:20:24 -0300

    Create 1001.cs

---
+++ c#/1001.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Uri {
+    class Uri {
+        static void Main(string[] args) {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int x = a + b;
+            Console.WriteLine("X = "+x);
+        }
+    }
+}
