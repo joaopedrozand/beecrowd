87209c12b10c816fc42f6cc7d139898cfb3d13e5
João Pedro Zandoná <33556037+joaopedrozand@users.noreply.github.com>
2021-12-23 19:20:54 -0300

    Create 1183.c

---
+++ c/1183.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(void){
+	char op;
+	float m[12][12],aux=0;
+	int i,j;
+	scanf("%c",&op);
+	for(i=0;i<12;i++){
+		for(j=0;j<12;j++){
+			scanf("%f",&m[i][j]);
+		}
+	}
+	for(i=0;i<11;i++){
+		for(j=i+1;j<12;j++)
+			aux+=m[i][j];
+	}
+	if(op=='S')
+		printf("%.1f\n",aux);
+	if(op=='M')
+		printf("%.1f\n",aux/66);
+	return 0;
+}
