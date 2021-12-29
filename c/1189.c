74b3387912bc1f68dc357c957813ceeaada4da52
João Pedro Zandoná <33556037+joaopedrozand@users.noreply.github.com>
2021-12-29 19:33:08 -0300

    Create 1189.c

---
+++ c/1189.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(void){
+	char op;
+	double m[12][12],aux=0;
+	int i,j;
+	scanf("%c",&op);
+	for(i=0;i<12;i++){
+		for(j=0;j<12;j++){
+			scanf("%lf",&m[i][j]);
+		}
+	}
+	for(j=0;j<5;j++){
+		for(i=j+1;i<12-(j+1);i++)
+			aux+=m[i][j];
+	}
+	if(op=='S')
+		printf("%.1lf\n",aux);
+	if(op=='M')
+		printf("%.1lf\n",aux/30);
+	return 0;
+}
